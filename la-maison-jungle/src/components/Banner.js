0abfa77a43e4a14e1aa152baf6be2c6130951e86
Orcryx <lauryannmagar@gmail.com>
2022-12-06 17:06:35 +0100

    Add style CSS pour banner.js

--- la-maison-jungle/src/components/Banner.js
+++ la-maison-jungle/src/components/Banner.js
@@ -1,8 +1,12 @@
+//importer la feuille de style css
 import '../styles/Banner.css';
+//Importer une image du dossier assets
+import logo from '../assets/logo.png';
 
 function Banner(){
     return (<div className='lmj-banner'>
-                <h1>La maison jungle</h1>
+                <img src={logo} alt='Logo de la maison de la jungle qui ressemble à une feuille verte' className='lmj-logo'/>
+                <h1 className='lmj-title'>La maison jungle</h1>
             </div>)
 }
 //Il s'agit d'une syntaxe prévue dans l'ES6, qui vous épargnera d'utiliser les accolades au moment de l'import. 
