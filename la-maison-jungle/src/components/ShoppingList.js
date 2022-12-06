d979b03611943894d84248e54fb0d758182bc50f
Orcryx <lauryannmagar@gmail.com>
2022-12-06 17:31:35 +0100

    Correction erreur console : créer une key pour chaque enfant de la liste à puce ul + check en console.log(ShoppingList())

--- la-maison-jungle/src/components/ShoppingList.js
+++ la-maison-jungle/src/components/ShoppingList.js
@@ -6,13 +6,17 @@ const plantList = [
     'palmier'
 ]
 
-/** Pour chaque élement dans la const PlantList, créer un élément li de la liste à puce ul cela en parcourant la liste avec la méthode map() */
+/** Pour chaque élement dans la const PlantList, créer un élément li de la liste à puce ul cela en parcourant la liste avec la méthode map()
+ * key : paramètre unique et à valeur stable dans le temps; ici créer avec une string et la combiner avec l'index de la data dans votre tableau map().
+ */
 function ShoppingList(){
     return(
         <ul> 
-            {plantList.map((plant)=>(<li>{plant}</li>))}
+            {plantList.map((plant, index)=>(<li key={`${plant}-${index}`}>{plant}</li>))}
         </ul>
+        
     )
 }
+//console.log(ShoppingList());
 
 export default ShoppingList;
