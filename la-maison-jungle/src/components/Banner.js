efc242153c09e768c6e8a0ad56cf567d63f54d40
Orcryx <lauryannmagar@gmail.com>
2022-12-06 16:36:12 +0100

    Dénugger le code suite aux suppressions et oublie d'import dans les autres fichiers

---
+++ la-maison-jungle/src/components/Banner.js
@@ -0,0 +1,5 @@
+function Banner(){
+    return <h1>La maison jungle</h1>
+}
+//Il s'agit d'une syntaxe prévue dans l'ES6, qui vous épargnera d'utiliser les accolades au moment de l'import. 
+export default Banner 
