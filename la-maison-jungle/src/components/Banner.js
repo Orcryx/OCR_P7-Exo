84e1e4c65b722707d7b1ac8f1dff2c82a66a9622
Orcryx <lauryannmagar@gmail.com>
2022-12-06 16:54:50 +0100

    Ajouter du css au composant banner

--- la-maison-jungle/src/components/Banner.js
+++ la-maison-jungle/src/components/Banner.js
@@ -1,7 +1,9 @@
 import '../styles/Banner.css';
 
 function Banner(){
-    return <h1>La maison jungle</h1>
+    return (<div className='lmj-banner'>
+                <h1>La maison jungle</h1>
+            </div>)
 }
 //Il s'agit d'une syntaxe prévue dans l'ES6, qui vous épargnera d'utiliser les accolades au moment de l'import. 
 export default Banner;
