1941b9035b94ea34668e55a4787f55707ae2bc50
Orcryx <lauryannmagar@gmail.com>
2022-12-06 16:49:03 +0100

    Ajouter un dossier 'style' y glisser les fichiers 'App.css' et 'Index.css' + MAJ des chemins vers ces fichiers

--- la-maison-jungle/src/components/Banner.js
+++ la-maison-jungle/src/components/Banner.js
@@ -2,4 +2,4 @@ function Banner(){
     return <h1>La maison jungle</h1>
 }
 //Il s'agit d'une syntaxe prévue dans l'ES6, qui vous épargnera d'utiliser les accolades au moment de l'import. 
-export default Banner 
+export default Banner;
