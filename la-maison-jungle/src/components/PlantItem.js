5a947fd9249675b5624528ad14c7707a43fe703c
Orcryx <lauryannmagar@gmail.com>
2022-12-07 14:54:24 +0100

    Déclencher un évement sur action click grace à une fonction + paramètre et une balise camelCase

--- la-maison-jungle/src/components/PlantItem.js
+++ la-maison-jungle/src/components/PlantItem.js
@@ -3,14 +3,15 @@ import '../styles/PlantItem.css'
 //importer un composant 
 import CareScale from './CareScale'
 
-function handleClick(){
-    console.log('Clic');
+function handleClick(plantName){
+    console.log(plantName);
+    //alert(`Vous voulez acheter 1 ${plantName} ?`)
 }
 
 
 function PlantItem({id, cover, name, water, light}){
     return (
-        <li key={id} className='lmj-plant-item' onClick={handleClick(name)}>
+        <li key={id} className='lmj-plant-item' onClick={() => handleClick(name)}>
             <img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`}/>
             {name}
             <div>
