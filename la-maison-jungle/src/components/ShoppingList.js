aef86393436ab21933949333b73f28f2718437e1
Orcryx <lauryannmagar@gmail.com>
2022-12-07 12:01:57 +0100

    Add style CSS pour ShoppingList.js/css

--- la-maison-jungle/src/components/ShoppingList.js
+++ la-maison-jungle/src/components/ShoppingList.js
@@ -20,9 +20,9 @@ function ShoppingList(){
 					<li key={cat}>{cat}</li>
 				))}
 			</ul>
-			<ul>
+			<ul className='lmj-plant-list'>
 				{plantList.map((plant) => (
-					<li key={plant.id}>{plant.name}{plant.isBestSale ? <span>🔥</span> : null}</li>
+					<li key={plant.id} className='lmj-plant-item'>{plant.name}{plant.isBestSale ? <span>🔥</span> : null}</li>
 				))}
 			</ul>
 		</div>
