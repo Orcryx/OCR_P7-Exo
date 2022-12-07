f5532ae28460ba6374b40124994942bbad1e6de2
Orcryx <lauryannmagar@gmail.com>
2022-12-07 11:59:40 +0100

    Add isSpecialOffer : false/true au datas de plantList.js

--- la-maison-jungle/src/datas/plantList.js
+++ la-maison-jungle/src/datas/plantList.js
@@ -3,54 +3,63 @@ export const plantList = [
 		name: 'monstera',
 		category: 'classique',
 		id: '1ed',
-		isBestSale: true
+		isBestSale: true,
+		isSpecialOffer : true
 	},
 	{
 		name: 'ficus lyrata',
 		category: 'classique',
 		id: '2ab',
-		isBestSale: true
+		isBestSale: true,
+		isSpecialOffer : false
 	},
 	{
 		name: 'pothos argenté',
 		category: 'classique',
 		id: '3sd',
-		isBestSale: null
+		isBestSale: null,
+		isSpecialOffer : true
 	},
 	{
 		name: 'yucca',
 		category: 'classique',
 		id: '4kk',
-		isBestSale: false
+		isBestSale: false,
+		isSpecialOffer : true
 	},
 	{
 		name: 'olivier',
 		category: 'extérieur',
 		id: '5pl',
-		isBestSale: false
+		isBestSale: false,
+		isSpecialOffer : true
 	},
 	{
 		name: 'géranium',
 		category: 'extérieur',
 		id: '6uo',
-		isBestSale: true
+		isBestSale: true,
+		isSpecialOffer : false
 	},
 	{
 		name: 'basilique',
 		category: 'extérieur',
 		id: '7ie',
-		isBestSale: true
+		isBestSale: true,
+		isSpecialOffer : true
 	},
 	{
 		name: 'aloe',
 		category: 'plante grasse',
 		id: '8fp',
-		isBestSale: true
+		isBestSale: true,
+		isSpecialOffer : false
 	},
 	{
 		name: 'succulente',
 		category: 'plante grasse',
 		id: '9vn',
-		isBestSale: true
+		isBestSale: true,
+		isSpecialOffer : false
 	}
 ]
